41091655b5d92356493491435d2ba06b2bc4fc4b
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-21 22:54:49 -0400

    Move pin -> subtree part 2

--- subtree/repin-conduit-binder.sh
+++ subtree/repin-conduit-binder.sh
@@ -2,4 +2,4 @@
 # this file cannot be marked executable for compatibility with overleaf
 
 # adapted from https://www.atlassian.com/git/tutorials/git-subtree
-git subtree pull --prefix pin/conduit-binder git@github.com:mmore500/conduit.git binder-artifacts --squash
+git subtree pull --prefix subtree/conduit-binder git@github.com:mmore500/conduit.git binder-artifacts --squash
